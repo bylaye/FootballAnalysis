954b51767181ed53a58980add5bbd413661c2f33
bylaye <abdoulayeniang2010@gmail.com>
2024-03-18 12:11:51 +0000

    function download dataset

--- download_season_data.py
+++ download_season_data.py
@@ -3,7 +3,9 @@ Download Season data Top European League.
 """
 
 import os
-from datetime import date
+from datetime import date, datetime
+import sys
+import requests
 
 """List Top League"""
 leagues = {
@@ -33,9 +35,39 @@ def season_list(number_season=1):
     # limit the max old season to 2005-2006
     if (year - number_season) < LIMIT_SEASON:
         number_season = year - LIMIT_SEASON
+        print(f'Warning: Limit season league = {LIMIT_SEASON}')
     for i in range(number_season):
         year_before = year-1
         season = str(year_before)[2:] + str(year)[2:]
         seasons.append(season)
         year -= 1
     return seasons
+
+
+def path_directory(parent=os.getcwd(), name='SeasonDatasets'):
+    path_dataset = os.path.join(parent, name)
+    if not os.path.isdir(path_dataset):
+        os.makedirs(path_dataset)
+    return path_dataset
+
+
+def download_datasets(league, season):
+    """Download match results by season grouped by league"""
+
+    url = "https://www.football-data.co.uk/mmz4281"
+    path_dataset = path_directory()
+    full_url = os.path.join(url, season, league)
+    try:
+        response = requests.get(full_url)
+        response.raise_for_status()
+        file_name = season+'.csv'
+        league_path = path_directory(path_dataset, league)
+        file_data = os.path.join(league_path, file_name)
+        with open(file_data, 'wb') as file:
+            file.write(response.content)
+    except requests.exceptions.RequestException as e:
+        print(e)
+    except IOError as e:
+        print(e)
+    except Exception as e:
+        print(e)
