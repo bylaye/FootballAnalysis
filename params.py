d378724a6fcff67dc87c13eb4baee97c3b3b6f12
bylaye <abdoulayeniang2010@gmail.com>
2024-03-18 18:50:23 +0000

    update params

--- params.py
+++ params.py
@@ -8,3 +8,10 @@ config.read(config_file)
 
 NAME_DIR_DATASET = config.get('dirs', 'DIR_NAME_DATASET')
 
+# db engine mysql/mariadb
+mysql = 'db_mysql'
+MYSQL_USER = config.get(mysql, 'USER')
+MYSQL_PASSWORD = config.get(mysql, 'PASSWORD')
+MYSQL_HOST = config.get(mysql, 'HOST')
+MYSQL_PORT = config.get(mysql, 'PORT')
+MYSQL_DB_NAME = config.get(mysql, 'DB_NAME')
